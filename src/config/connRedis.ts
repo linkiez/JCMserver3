a94a8e3c6d66d557fd682f3db5441af3868e3922
Fabio Moreno <linkiez@gmail.com>
2022-07-06 17:35:11 -0300

    config Redis

--- src/config/connRedis.ts
+++ src/config/connRedis.ts
@@ -1,9 +1,21 @@
 import redis from 'redis';
+import dotenv from "dotenv";
+dotenv.config();
 
+let username = process.env.REDIS_USERNAME
+let password = process.env.REDIS_PASSWORD
+let host = process.env.REDIS_HOST || 'localhost'
+let port = process.env.REDIS_PORT || ''
+let url = 'redis://';
+
+if(username) url = url + username + ':' + password + '@'
+url = url + host;
+if(port) url = url + ':' + port;
+url = url + '/';
 
 const client = redis.createClient({
     //redis[s]://[[username][:password]@][host][:port][/db-number]
-    url: 'redis://localhost:6379/'
+    url: url
   })
 
   client.connect()
