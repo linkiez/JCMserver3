3710dce373dca70418c8b544c7a4cc28577996d2
Fabio Moreno <linkiez@gmail.com>
2022-08-02 11:38:43 -0300

    updateOrdemProducao

--- src/controllers/ordemProducaoController.ts
+++ src/controllers/ordemProducaoController.ts
@@ -130,33 +130,147 @@ export default class OrdemProducaoController {
       );
 
       Promise.all(ordemProducaoItens).then(async () => {
-        
         let ordemProducaoCreated2 = await OrdemProducao.findByPk(
-          ordemProducaoCreated.id);
-        
+          ordemProducaoCreated.id
+        );
+
         await transaction.commit();
 
         ordemProducaoCreated2 = await OrdemProducao.findByPk(
-            ordemProducaoCreated.id,
-            {
-              include: [
-                Vendedor,
-                {
-                  model: OrdemProducaoItem,
-                  include: [
-                    FileDb,
-                    {
-                      model: OrdemProducaoItemProcesso,
-                      attributes: { exclude: ["id_ordem_producao_item"] },
-                    },
-                  ],
-                  attributes: { exclude: ["id_ordem_producao"] },
-                },
-              ],
-              attributes: { exclude: ["id_vendedor"] },
+          ordemProducaoCreated.id,
+          {
+            include: [
+              Vendedor,
+              {
+                model: OrdemProducaoItem,
+                include: [
+                  FileDb,
+                  {
+                    model: OrdemProducaoItemProcesso,
+                    attributes: { exclude: ["id_ordem_producao_item"] },
+                  },
+                ],
+                attributes: { exclude: ["id_ordem_producao"] },
+              },
+            ],
+            attributes: { exclude: ["id_vendedor"] },
+          }
+        );
+        return res.status(201).json(ordemProducaoCreated2);
+      });
+    } catch (error: any) {
+      await transaction.rollback;
+      console.log(error);
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async updateOrdemProducao(req: Request, res: Response) {
+    const transaction = await sequelize.transaction();
+    try {
+      const { id } = req.params;
+      let ordemProducao = req.body;
+      let ordemProducaoItens: any = ordemProducao.ordem_producao_items;
+      delete ordemProducao.ordem_producao_items;
+
+      if (ordemProducao.orcamento)
+        ordemProducao.id_orcamento = ordemProducao.orcamento.id;
+      if (ordemProducao.vendedor)
+        ordemProducao.id_vendedor = ordemProducao.vendedor.id;
+
+      delete ordemProducao.orcamento;
+      delete ordemProducao.vendedor;
+
+      await OrdemProducao.update(ordemProducao, {
+        where: { id: Number(id) },
+        transaction: transaction,
+      });
+
+      await OrdemProducaoItem.destroy({
+        where: { id_ordem_producao: Number(id) },
+        transaction: transaction,
+      });
+
+      ordemProducaoItens = ordemProducaoItens.map(
+        async (ordemProducaoItem: any) => {
+          let files = ordemProducaoItem.files;
+          delete ordemProducaoItem.files;
+
+          if (ordemProducaoItem.produto) {
+            ordemProducaoItem.id_produto = ordemProducaoItem.produto.id;
+            delete ordemProducaoItem.produto;
+          }
+
+          if (ordemProducaoItem.rir) {
+            ordemProducaoItem.id_rir = ordemProducaoItem.rir.id;
+            delete ordemProducaoItem.rir;
+          }
+
+          ordemProducaoItem.id_ordem_producao = id;
+
+          let OrdemProducaoItemProcessos =
+            ordemProducaoItem.ordem_producao_item_processos;
+          delete ordemProducaoItem.ordem_producao_item_processos;
+
+          let ordemProducaoItemCreated = await OrdemProducaoItem.create(
+            ordemProducaoItem,
+            { transaction: transaction }
+          );
+
+          if (files) {
+            await ordemProducaoItemCreated.setFiles(
+              files.map((item: any) => item.id),
+              { transaction: transaction }
+            );
+          }
+
+          OrdemProducaoItemProcessos = OrdemProducaoItemProcessos.map(
+            async (ordemProducaoItemProcesso: any) => {
+              ordemProducaoItemProcesso.id_ordem_producao_item =
+                ordemProducaoItemCreated.id;
+
+              let OrdemProducaoItemProcessoCreated =
+                await OrdemProducaoItemProcesso.create(
+                  ordemProducaoItemProcesso,
+                  { transaction: transaction }
+                );
+
+              return OrdemProducaoItemProcessoCreated;
             }
           );
-        return res.status(201).json(ordemProducaoCreated2);
+
+          return ordemProducaoItemCreated;
+        }
+      );
+
+      Promise.all(ordemProducaoItens).then(async () => {
+        let ordemProducaoUpdated = await OrdemProducao.findByPk(
+          id
+        );
+
+        await transaction.commit();
+
+        ordemProducaoUpdated = await OrdemProducao.findByPk(
+          id,
+          {
+            include: [
+              Vendedor,
+              {
+                model: OrdemProducaoItem,
+                include: [
+                  FileDb,
+                  {
+                    model: OrdemProducaoItemProcesso,
+                    attributes: { exclude: ["id_ordem_producao_item"] },
+                  },
+                ],
+                attributes: { exclude: ["id_ordem_producao"] },
+              },
+            ],
+            attributes: { exclude: ["id_vendedor"] },
+          }
+        );
+        return res.status(202).json(ordemProducaoUpdated);
       });
     } catch (error: any) {
       await transaction.rollback;
--- src/routes/ordemProducaoRoutes.ts
+++ src/routes/ordemProducaoRoutes.ts
@@ -7,7 +7,7 @@ router
   .get("/ordemproducao", OrdemProducaoController.findAllOrdemProducao)
   .get("/ordemproducao/deleted", OrdemProducaoController.findAllOrdemProducaoDeleted)
   .get("/ordemproducao/:id", OrdemProducaoController.findOneOrdemProducao)
-  //.put("/ordemproducao/:id", OrdemProducaoController.updateOrdemProducao)
+  .put("/ordemproducao/:id", OrdemProducaoController.updateOrdemProducao)
   .post("/ordemproducao/restore/:id", OrdemProducaoController.restoreOrdemProducao)
   .delete("/ordemproducao/:id", OrdemProducaoController.destroyOrdemProducao)
 
