d5074c3ef430695ffc0c6e64c8ad752c43ecc7a7
Fabio Moreno <linkiez@gmail.com>
2023-01-02 13:54:26 -0300

    vendedor findall query

--- src/controllers/vendedorController.ts
+++ src/controllers/vendedorController.ts
@@ -1,15 +1,58 @@
 import { Request, Response } from "express";
 import Vendedor from "../models/Vendedor";
 import Pessoa from "../models/Pessoa";
+import { Op } from "sequelize";
 
 export default class VendedorController {
   static async findAllVendedors(req: Request, res: Response) {
     try {
-      const vendedor = (await Vendedor.findAll({
-        include: [Pessoa],
+      let consulta: any = {
+        pageCount: Number(req.query.pageCount) || 10,
+        page: Number(req.query.page) || 0,
+        searchValue: req.query.searchValue,
+      };
+
+      let resultado: { vendedores: Vendedor[]; totalRecords: Number } = {
+        vendedores: [],
+        totalRecords: 0,
+      };
+
+      let queryWhere: any = {
+        nome: { [Op.like]: "%" + consulta.searchValue + "%" },
+      };
+
+      if (req.query.deleted === "true")
+        queryWhere = { ...queryWhere, deletedAt: { [Op.not]: null } };
+
+      resultado.vendedores = await Vendedor.findAll({
+        limit: consulta.pageCount,
+        offset: consulta.pageCount * consulta.page,
+        include: [
+          {
+            model: Pessoa,
+            where:
+              consulta.searchValue !== "undefined"
+                ? queryWhere
+                : undefined,
+          },
+        ],
+        paranoid: req.query.deleted === "true" ? false : true,
         attributes: { exclude: ["id_pessoa"] },
-      }));
-      return res.status(200).json(vendedor);
+      });
+      resultado.totalRecords = await Vendedor.count({
+        include: [
+          {
+            model: Pessoa,
+            where:
+              consulta.searchValue !== "undefined"
+                ? queryWhere
+                : undefined,
+          },
+        ],
+        paranoid: req.query.deleted === "true" ? false : true,
+      });
+
+      return res.status(200).json(resultado);
     } catch (error: any) {
       console.log(error);
       return res.status(500).json(error.message);
@@ -19,11 +62,11 @@ export default class VendedorController {
   static async findOneVendedor(req: Request, res: Response) {
     const { id } = req.params;
     try {
-      const vendedor = (await Vendedor.findOne({
+      const vendedor = await Vendedor.findOne({
         where: { id: Number(id) },
         include: [Pessoa],
         attributes: { exclude: ["id_pessoa"] },
-      }));
+      });
       return res.status(200).json(vendedor);
     } catch (error: any) {
       console.log(error);
@@ -38,9 +81,7 @@ export default class VendedorController {
       delete vendedor.pessoa;
     }
     try {
-      const vendedorCreated = (await Vendedor.create(
-        vendedor
-      ));
+      const vendedorCreated = await Vendedor.create(vendedor);
       return res.status(201).json(vendedorCreated);
     } catch (error: any) {
       console.log(error);
@@ -58,9 +99,9 @@ export default class VendedorController {
     delete vendedor.id;
     try {
       await Vendedor.update(vendedor, { where: { id: Number(id) } });
-      const vendedorUpdated = (await Vendedor.findOne({
+      const vendedorUpdated = await Vendedor.findOne({
         where: { id: Number(id) },
-      }));
+      });
       return res.status(202).json(vendedorUpdated);
     } catch (error: any) {
       console.log(error);
