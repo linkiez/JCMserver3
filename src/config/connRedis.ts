8ba8b8872f8dd49ba09033daa256e0e187789697
Fabio Moreno <linkiez@gmail.com>
2023-05-29 08:04:37 -0300

    redis config

--- src/config/connRedis.ts
+++ src/config/connRedis.ts
@@ -6,14 +6,21 @@ let password = process.env.REDIS_PASSWORD
 let host = process.env.REDIS_HOST || 'localhost'
 let port = Number(process.env.REDIS_PORT)
 
-const client = redis.createClient({
-    socket: { 
-      host: host,
-      port: port
-    },
-    password: password
-  })
+let client
 
-  client.connect()
+if (process.env.REDIS_URL) {
+    client = redis.createClient({
+      url: process.env.REDIS_URL
+    })
+} else {
+    client = redis.createClient({
+      socket: { 
+        host: host,
+        port: port
+      },
+      password: password
+    })
+}
 
+client.connect()
 export default client
