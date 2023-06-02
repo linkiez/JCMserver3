3d6152b568e421a9e2f3e2e731a408eb2b299bbf
Fabio Moreno <linkiez@gmail.com>
2023-06-02 13:14:48 -0300

    ajust retry_strategy redis

--- src/config/connRedis.ts
+++ src/config/connRedis.ts
@@ -1,26 +1,52 @@
-import redis, { RedisClientType } from 'redis';
+import redis, { RedisClientType } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 
-let password = process.env.REDIS_PASSWORD
-let host = process.env.REDIS_HOST || 'localhost'
-let port = Number(process.env.REDIS_PORT)
+let password = process.env.REDIS_PASSWORD;
+let host = process.env.REDIS_HOST || "localhost";
+let port = Number(process.env.REDIS_PORT);
 
-let client: RedisClientType
+let client: RedisClientType;
 
 if (process.env.REDIS_URL) {
-    client = redis.createClient({
-      url: process.env.REDIS_URL
-    })
+  const clientOptions = {
+    url: process.env.REDIS_URL,
+    retry_strategy: (options: any) => {
+      if (options.error && options.error.code === "ECONNREFUSED") {
+        return new Error("The server refused the connection");
+      }
+      if (options.total_retry_time > 1000 * 60 * 60) {
+        return new Error("Retry time exhausted");
+      }
+      if (options.attempt > 10) {
+        return undefined;
+      }
+      return Math.min(options.attempt * 100, 3000);
+    },
+  };
+
+  client = redis.createClient(clientOptions);
 } else {
-    client = redis.createClient({
-      socket: { 
-        host: host,
-        port: port
-      },
-      password: password
-    })
+  const clientOptions = {
+    socket: {
+      host: host,
+      port: port,
+    },
+    password: password,
+    retry_strategy: (options: any) => {
+      if (options.error && options.error.code === "ECONNREFUSED") {
+        return new Error("The server refused the connection");
+      }
+      if (options.total_retry_time > 1000 * 60 * 60) {
+        return new Error("Retry time exhausted");
+      }
+      if (options.attempt > 10) {
+        return undefined;
+      }
+      return Math.min(options.attempt * 100, 3000);
+    },
+  };
+  client = redis.createClient(clientOptions);
 }
 
-client.connect()
-export default client
+export default client;
