5e4af01d17795d3efcfcc943d7d8ffab4456fd72
LiNkIeZ <linkiez@gmail.com>
2023-09-06 11:21:06 -0300

    remove captcha key

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -4,34 +4,21 @@ import nodemailer from "nodemailer";
 export default class EmailController {
   static async sendEmail(req: Request, res: Response) {
     try {
-      if (!req.body) {
-        return res.status(400).json({ message: "Formulario Vazio" });
-      }
+      if (!req.body) throw new Error("Requisição inválida");
 
-      if (!req.body.name) {
-        return res.status(400).json({ message: "Nome não informado" });
-      }
+      if (!req.body.name) throw new Error("Form: Nome não informado");
 
-      if (!req.body.email) {
-        return res.status(400).json({ message: "E-mail não informado" });
-      }
+      if (!req.body.email) throw new Error("Form: E-mail não informado");
 
-      if (!req.body.tel) {
-        return res.status(400).json({ message: "Telefone não informado" });
-      }
+      if (!req.body.tel) throw new Error("Form: Telefone não informado");
 
-      if (!req.body.message) {
-        return res.status(400).json({ message: "Mensagem não informada" });
-      }
+      if (!req.body.message) throw new Error("Form: Mensagem não informada");
 
-      if (!req.body.token) {
-        return res.status(400).json({ message: "Token não informado" });
-      }
+      if (!req.body.token) throw new Error("Form: Token não informado");
 
       const validoCaptha = await validateCaptcha(req.body.token);
       if (!validoCaptha) {
-
-        return res.status(400).json({ message: "Token inválido" });
+        throw new Error("Form: Token inválido");
       }
 
       let transporter = nodemailer.createTransport({
@@ -70,7 +57,7 @@ export default class EmailController {
 }
 
 async function validateCaptcha(response_key: string) {
-  const secret_key = "6Le9dPsnAAAAAJgezv5bCRjbyVAq8jmM2gCEIdDi";
+  const secret_key = process.env.CAPTCHA_SECRET_KEY;
 
   const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${response_key}`;
 
