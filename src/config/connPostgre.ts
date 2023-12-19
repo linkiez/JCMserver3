d4a71d1abf2e053f3e8323edfbbff90bfcf679c0
LiNkIeZ <linkiez@gmail.com>
2023-12-19 09:45:20 -0300

    Database Pool

--- src/config/connPostgre.ts
+++ src/config/connPostgre.ts
@@ -3,30 +3,33 @@ import { Options } from "sequelize/types";
 import dotenv from "dotenv";
 dotenv.config();
 
-var config: Options = {
+let config: Options = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASS,
   database: process.env.DATABASE_NAME,
   host: process.env.DATABASE_HOST,
   dialect: "postgres",
-  minifyAliases:  true
+  minifyAliases:  true,
+  pool: {
+    max: 20,
+    min: 0,
+    idle: 10000,
+    acquire: 30000,
+  },
+  logging: console.log,
 };
 
 if (process.env.NODE_ENV == "production") config.logging = false;
 
 let sequelize: Sequelize;
 if (process.env.NODE_ENV == "test") {
-  sequelize = new Sequelize('sqlite::memory:', { logging: false });
+  sequelize = new Sequelize('sqlite::memory:', { logging: true });
 }else{
   sequelize = new Sequelize(
     config.database!,
     config.username!,
     config.password,
-    process.env.NODE_ENV != 'test' ? config : {
-      dialect: "sqlite",
-      storage: ":memory:",
-      logging: false
-    }
+    config
   );
 }
 
