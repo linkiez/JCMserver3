bd973f54481912ac56c2d381924c49a9b0458b8f
LiNkIeZ <linkiez@gmail.com>
2023-12-20 08:36:42 -0300

    sql update

--- sql/ordem_producao_item_view.sql
+++ sql/ordem_producao_item_view.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE VIEW ordem_producao_item_view AS
+CREATE
+OR REPLACE VIEW ordem_producao_item_view AS
 SELECT
 	p.nome AS cliente,
 	pv.nome AS vendedor,
@@ -13,12 +14,18 @@ SELECT
 	oi.total,
 	oi.custo,
 	oi.id_orcamento,
-	op."status"
-FROM ordem_producao_item opi
-LEFT JOIN produto pr ON opi.id_produto = pr.id
-LEFT JOIN orcamento_item oi ON opi.id_orcamento_item = oi.id
-LEFT JOIN orcamento orc ON oi.id_orcamento = orc.id
-LEFT JOIN pessoa p ON orc.id_pessoa = p.id
-LEFT JOIN vendedor v ON orc.id_vendedor = v.id
-LEFT JOIN pessoa pv ON v.id_pessoa = pv.id
-LEFT JOIN ordem_producao op ON opi.id_ordem_producao = op.id;
+	op."status",
+	op."createdAt",
+	op."updatedAt",
+	op.data_finalizacao,
+	op.data_entregue,
+	orc.desconto
+FROM
+	ordem_producao_item opi
+	LEFT JOIN produto pr ON opi.id_produto = pr.id
+	LEFT JOIN orcamento_item oi ON opi.id_orcamento_item = oi.id
+	LEFT JOIN orcamento orc ON oi.id_orcamento = orc.id
+	LEFT JOIN pessoa p ON orc.id_pessoa = p.id
+	LEFT JOIN vendedor v ON orc.id_vendedor = v.id
+	LEFT JOIN pessoa pv ON v.id_pessoa = pv.id
+	LEFT JOIN ordem_producao op ON opi.id_ordem_producao = op.id;
